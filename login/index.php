903c1c9d1742e271ff0731b6e36e578c3d00e9df
한재우 <hanjay3757@gmail.com>
2025-08-06 17:12:09 +0900

    format

--- login/index.php
+++ login/index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="ko">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,9 +10,10 @@
     -->
     <script src="login.js"></script>
 </head>
+
 <body>
     <h1>JWT 로그인 시스템</h1>
-    
+
     <!-- 
         로그인 폼
     -->
@@ -19,15 +21,16 @@
         <label for="id">아이디</label>
         <input type="text" name="id" id="id" required>
         <br>
-        
+
         <label for="pw">비밀번호</label>
         <input type="password" name="pw" id="pw" required>
         <br>
-        
+
         <button id="login_btn" type="submit">로그인</button>
     </form>
-    
+
     <a href="member.php">회원전용페이지</a>
-    
+
 </body>
+
 </html>
